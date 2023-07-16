fac406b9338bfa5fb72ee6cf8970b01030d9b7d6
Pascal Polleunus <pascal@polleunus.be>
2023-07-16 16:54:09 +0200

    Rename variable.

--- bookmarklets/gmail-clean-print.js
+++ bookmarklets/gmail-clean-print.js
@@ -1,12 +1,1 @@
-// USAGE: Gmail › Open email › Print › Cancel › Bookmarklet
-
-[
-  'body > div > table',
-  'body > div > hr',
-  'body > div > div > table:nth-child(1)',
-  'body > div > div > hr',
-  'body > div > div > table.message > tbody > tr:nth-child(1)',
-  'body > div > div > table.message > tbody > tr:nth-child(2)',
-].reverse().forEach((s) => { document.querySelector(s).remove(); });
-
-window.print();
+javascript:["body > div > div > table.message > tbody > tr:nth-child(2)","body > div > div > table.message > tbody > tr:nth-child(1)","body > div > div > hr","body > div > div > table:nth-child(1)","body > div > hr","body > div > table"].forEach(a=>{document.querySelector(a).remove()}),window.print();
---
+++ bookmarklets/src/gmail-clean-print.js
@@ -0,0 +1,12 @@
+// USAGE: Gmail › Open email › Print › Cancel › Bookmarklet
+
+[
+  'body > div > table',
+  'body > div > hr',
+  'body > div > div > table:nth-child(1)',
+  'body > div > div > hr',
+  'body > div > div > table.message > tbody > tr:nth-child(1)',
+  'body > div > div > table.message > tbody > tr:nth-child(2)',
+].reverse().forEach((s) => { document.querySelector(s).remove(); });
+
+window.print();
