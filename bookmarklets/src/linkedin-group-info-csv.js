eedf29be1675d9db0b77f59e176a357a065c6bb5
Pascal Polleunus <pascal@polleunus.be>
2023-08-05 19:31:33 +0200

    Add bookmarklet `LinkedIn Group Info CSV`.

---
+++ bookmarklets/src/linkedin-group-info-csv.js
@@ -0,0 +1,25 @@
+// Scrape info about the current LinkedIn group.
+//
+// USAGE: LinkedIn Group page › Bookmarklet
+//
+// Columns: Name, URL, Description, Members.
+
+
+function g(s, p) {
+  p = p || document;
+  e = p.querySelector(s);
+  return e ? e.textContent.trim() : '';
+}
+
+d=[];
+d.push(g('h1.groups-entity__name > span'));  // Name
+d.push(window.location.href);  // URL
+d.push(g('.groups-guest-view__truncate span'));  // Description
+d.push(g('section.groups-header__container > div div.mt1').replace(' members', '').replace(/[^0-9]/g, ''));  // Members
+
+e = document.createElement('textarea');
+e.value = d.join('\t');
+console.log(e.value);
+document.body.appendChild(e);
+e.select();
+document.execCommand('copy');
