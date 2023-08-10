fa845e9ea1cfda5a7eeedd98f2eb91232d926d5e
Pascal Polleunus <pascal@polleunus.be>
2023-08-10 16:09:27 +0200

    Cosmetic changes.

--- bookmarklets/src/twitter-disable-interests.js
+++ bookmarklets/src/twitter-disable-interests.js
@@ -8,14 +8,18 @@
 u = 'https://twitter.com/settings/your_twitter_data/twitter_interests';
 if (window.location.href != u) navigation.navigate(u);
 timer = 1000;
+
 items = document.querySelectorAll('div > input[type="checkbox"]:checked');
 if (items.length == 0) alert('🤷‍♂️ All interests already disabled.');
-items.forEach((interest, i) => {
-  setTimeout(function() {
+
+items.forEach(function (interest, i) {
+  setTimeout(function () {
     ii = i + 1;
     console.log('Disabling interest ' + ii + '/' + items.length);
+
     interest.click();
     interest.scrollIntoView();
+
     if (ii == items.length) alert('👍 All interests have been disabled.');
   }, timer);
   timer += 2000;
