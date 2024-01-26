47f21943fd6f718cf0bc0b7b461cbedb3096cf82
Pascal Polleunus <pascal@polleunus.be>
2024-01-26 15:02:35 +0100

    Review `article-link` according to template and helpers.

--- userscripts/article-link.js
+++ userscripts/article-link.js
@@ -2,12 +2,15 @@
 // @name         Article Link
 // @description  Create a Markdown string with information about the article, and copy it to the clipboard.
 // @icon         data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔗</text></svg>
-// @version      24012601
+// @version      24012602
 // @namespace    ppo
-// @author       ppo
+// @author       Pascal Polleunus <https://pascal.polleunus.be>
 // @match        *://*/*
 // @run-at       context-menu
 // ==/UserScript==
+
+
+// DOCUMENTATION ===================================================================================
 
 // USAGE: Go to the article page. Execute this userscript.
 //
@@ -19,34 +22,19 @@
 //   [Tampermonkey](https://www.tampermonkey.net/)
 //   Update URL: https://raw.githubusercontent.com/ppo/gist/HEAD/userscripts/article-link.js
 
-function dateFormat(value) {
-  let d = new Date(value);
-  return [d.getUTCFullYear(), d.getUTCMonth()+1, d.getUTCDate()].reduce(
-    (accumulator, chunk) => accumulator + `0${chunk}`.slice(-2),
-    ''
-  );
-}
 
-function findElement(tags) {
-  for (let tag of tags) {
-    for (let namespace of ['main', 'article', 'section', '']) {
-      let e = document.querySelector(`${namespace} ${tag}`);
-      if (e) return e;
-    }
-  }
-}
+// CONSTANTS =======================================================================================
 
-function getHeading() {
-  let e = findElement(['h1', 'h2']);
-  if (e) return e.innerText.trim();
-}
+const NAMESPACES = ['main', 'article', 'section', ''];
+const HEADING_TAGS = ['h1', 'h2'];
+const TIME_TAGS = ['time'];
 
-function getTime() {
-  let e = findElement(['time']);
-  if (e) return dateFormat(e.dateTime);
-}
+
+// SHARED HELPERS ==================================================================================
 
 function copyToClipboard(value) {
+  // Copies the given value to the clipboard.
+  // _utils.js / version: 24012601
   let e = document.createElement('textarea');
   e.value = value;
   document.body.appendChild(e);
@@ -54,12 +42,50 @@ function copyToClipboard(value) {
   document.execCommand('copy');
 }
 
+function dateFormat(value) {
+  // Formats a date (Date or string) as YYMMDD.
+  // _utils.js / version: 24012601
+  let d = new Date(value);
+  return [d.getUTCFullYear(), d.getUTCMonth()+1, d.getUTCDate()].reduce(
+    (accumulator, chunk) => accumulator + `0${chunk}`.slice(-2),
+    ''
+  );
+}
+
+function findFirstElement(selectors, namespaces) {
+  // Finds the first element matching a series of selectors, located under a series of namespaces.
+  // _utils.js / version: 24012601
+  for (let selector of selectors) {
+    for (let namespace of namespaces) {
+      let e = document.querySelector(`${namespace} ${selector}`);
+      if (e) return e;
+    }
+  }
+}
+
+
+// LOCAL HELPERS ===================================================================================
+
+function getHeading() {
+  let e = findFirstElement(HEADING_TAGS, NAMESPACES);
+  if (e) return e.innerText.trim();
+}
+
+function getTime() {
+  let e = findFirstElement(TIME_TAGS, NAMESPACES);
+  if (e) return dateFormat(e.dateTime);
+}
+
+
+// MAIN ============================================================================================
+
 (function() {
   'use strict';
 
   let title = window.getSelection().toString().trim() || getHeading();
-  if (!title) alert('Article title not found. Select it.');
-  else {
+  if (!title) {
+    alert('Article title not found. Select it.');
+  } else {
     let url = window.location.href;
     let date = getTime();
     let result = `[${title}](${url})` + (date ? ` (${date})` : '');
