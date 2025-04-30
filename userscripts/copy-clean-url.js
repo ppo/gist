09772f5249f1425447163dce6b5c1b1a191b1003
Pascal Polleunus <pascal@polleunus.be>
2025-04-30 14:28:19 +0300

    Add `snackbar()`. Use it in `copyToClipboard()`.

--- userscripts/copy-clean-url.js
+++ userscripts/copy-clean-url.js
@@ -22,14 +22,65 @@ let specialSite = null;
 
 // SHARED HELPERS ==================================================================================
 
-function copyToClipboard(value) {
+function copyToClipboard(value, message=true, timeout=undefined) {
   // Copy the given value to the clipboard.
-  // _utils.js / version: 240126-01
+  // _utils.js / version: 250430-01
   const e = document.createElement('textarea');
   e.value = value;
   document.body.appendChild(e);
   e.select();
+
   document.execCommand('copy');
+  document.body.removeChild(e);
+
+  if (message) {
+    if (message === true) {
+      message = document.createElement('div');
+
+      const heading = document.createElement('p');
+      heading.style = 'font-weight: bold; margin-bottom: 1rem';
+      heading.textContent = 'Value copied to clipboard:';
+      message.appendChild(heading);
+
+      const pre = document.createElement('pre');
+      pre.textContent = value;
+      message.appendChild(pre);
+
+      message = message.innerHTML;
+    }
+    snackbar(message, timeout);
+  }
+}
+
+function snackbar(message, timeout=2000) {
+  // Display a temporary message.
+  // Or fixed until clicked if `!timeout`.
+  // _utils.js / version: 250430-01
+  const elem = document.createElement('div');
+  elem.innerHTML = message;
+  Object.assign(elem.style, {
+    all: 'initial',
+    backgroundColor: '#ffca28',
+    border: '3px solid #fff',
+    boxShadow: 'rgba(0, 0, 0, 0.5) 3px 6px 12px',
+    color: '#000',
+    fontFamily: 'sans-serif',
+    left: '50%',
+    maxHeight: '75%',
+    maxWidth: '75%',
+    minWidth: '250px',
+    overflow: 'auto',
+    padding: '1rem 1.25rem',
+    position: 'fixed',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+    zIndex: '9999',
+  });
+  document.body.appendChild(elem);
+
+  const destroy = () => { document.body.removeChild(elem); };
+  elem.addEventListener('click', destroy);
+  if (timeout) setTimeout(destroy, timeout);
 }
 
 
