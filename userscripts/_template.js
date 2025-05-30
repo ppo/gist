4dd51d97c849d42a8f224a9de4f8b16953140186
Pascal Polleunus <pascal@polleunus.be>
2025-05-30 23:40:22 +0300

    Minor change.

--- userscripts/_template.js
+++ userscripts/_template.js
@@ -2,7 +2,7 @@
 // @name         {{SCRIPT_NAME}}
 // @description  {{SCRIPT_DESCRIPTION}}
 // @icon         data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔗</text></svg>
-// @version      {{YYMMDDnn}}
+// @version      {{YYMMDD-nn}}
 // @namespace    ppo
 // @author       Pascal Polleunus <https://pascal.polleunus.be>
 // @match        *://*/*
