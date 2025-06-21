7b87a1f871b4047d8cbf3304200a0abdfacfc9cc
Pascal Polleunus <pascal@polleunus.be>
2025-06-21 14:50:05 +0200

    Add script to create a calendar event for flights on Booking.com.

---
+++ userscripts/booking-flight-info.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         Booking.com: Flight Info
+// @description  Prepare flight info for calendar
+// @icon         data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔗</text></svg>
+// @version      {{YYMMDD-nn}}
+// @namespace    ppo
+// @author       Pascal Polleunus <https://pascal.polleunus.be>
+// @match        *://*/*
+// @require      https://raw.githubusercontent.com/ppo/gist/refs/heads/master/userscripts/_utils.js
+// @run-at       context-menu | document-start | document-end
+// ==/UserScript==
+
+
+// DOCUMENTATION ===================================================================================
+
+// USAGE: {{USAGE}}
+//
+// USERSCRIPTS MANAGER:
+//   [Tampermonkey](https://www.tampermonkey.net/)
+//   Update URL: https://raw.githubusercontent.com/ppo/gist/HEAD/userscripts/{{FILENAME}}.js
+
+
+// SETTINGS ========================================================================================
+
+// const waldo = 'constant value';
+
+
+// HELPERS =========================================================================================
+
+// function bar() {
+// }
+
+
+// MAIN ============================================================================================
+
+(function() {
+  'use strict';
+
+  // Main code.
+
+})();
